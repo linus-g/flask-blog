60ae00beaad682b6e452df28055432a51ec969ef
Linus Gustafsson <linus.gustafsson@gmail.com>
2020-10-10 13:32:47 +1100

    added showing posts on main page

--- blog.py
+++ blog.py
@@ -1,9 +1,11 @@
 # blog.py - controller
 
 # imports
-from flask import Flask, render_template, request, session, \
-    flash, redirect, url_for
 import sqlite3
+from functools import wraps
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+
 
 # configuration
 DATABASE = 'blog.db'
@@ -18,7 +20,18 @@ app.config.from_object(__name__)
 
 # function used for connecting to the database
 def connect_db():
-    return sqlite3.connect(app.config['DATEBASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,8 +50,13 @@ def login():
 
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cursor = g.db.execute('SELECT * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cursor.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 
 @app.route('/logout')
